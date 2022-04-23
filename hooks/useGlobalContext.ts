9a10159680595a84c20c26278fa28f7e36763ec6
ochikov <ognyan@limechain.tech>
2022-04-23 14:16:44 +0300

    fix for the global context - added hook

---
+++ hooks/useGlobalContext.ts
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { GlobalContext, UpdateGlobalContext } from "../context/GlobalContext";
+
+
+export function useGlobalContext() {
+  const globalContext = useContext(GlobalContext);
+  const updateGlobalContext = useContext(UpdateGlobalContext);
+
+  return {
+    ...globalContext,
+    ...updateGlobalContext,
+  };
+}
