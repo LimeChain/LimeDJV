566ee4d4cdd0cbce5342ee4c89ce5a00211c26a7
ochikov <ognyan@limechain.tech>
2022-04-23 12:10:12 +0300

    added style for title component

--- components/Shared/Title.tsx
+++ components/Shared/Title.tsx
@@ -8,6 +8,18 @@ const Title = ({ text }) => {
         <h2>{text}</h2>
       </div>
      
+      <style jsx>{`
+        .title-wrapper {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+        h2 {
+          font-weight: 600;
+          font-size: 24px;
+          color: #25067d;
+        }
+      `}</style>
     </div>
   );
 };
