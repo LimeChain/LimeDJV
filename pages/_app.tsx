b5e32236e232cab74f211e0bf06109b1c992a5e6
ochikov <ognyan@limechain.tech>
2022-04-23 13:53:30 +0300

    added global context and wrap the App with it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { GlobalProvider } from "../context/GlobalContext";
+import "react-toggle/style.css"; // for ES6 modules
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function NextWeb3App({ Component, pageProps }: AppProps) {
+  return (
+    <GlobalProvider>
+      <Component {...pageProps} />
+    </GlobalProvider>
+  );
 }
 
-export default MyApp
+export default NextWeb3App;
